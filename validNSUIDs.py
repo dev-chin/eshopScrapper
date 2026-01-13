fdf03be0bf018bc4bf45a566789e443290558fbb
dev-chin <59340654+dev-chin@users.noreply.github.com>
2026-01-13 18:19:58 +0100

    Update validNSUIDs.py

--- validNSUIDs.py
+++ validNSUIDs.py
@@ -6,25 +6,15 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 import threading
 
-# Authorization token
-glob_headers = {}
-
-callcount = 0
-
-url = "https://api.ec.nintendo.com/v1/price"
-
-base_nsuid = 70010000000000
-
 REGIONS = ["JP", "US", "HK", "BR", "CA", "AR", "CL", "CO", "PE", "MX", "AU", "NZ"]
-
 #Regions that we currently don't know how to extract titleids from
 #REGIONS += ["BG", "CH", "CY", "EE", "HR", "IE", "LT", "LU", "LV", "MT", "RO", "SI", "SK", "AT", "BE", "CZ", "DK", "ES", "FI", "GR", "HU", "NL", "NO", "PL", "PT", "RU", "ZA", "SE", "IT", "FR", "DE", "GB"]
-
-os.makedirs("ValidNsuIds", exist_ok=True)
 
 check_at_once = 50 # Max before site returns "Over ids limit number"
 
 def addToNSUIDs(region: str):
+    base_nsuid = 70010000000000
+    url = "https://api.ec.nintendo.com/v1/price"
     params = {
         "country": region,
         "ids": [],
@@ -57,5 +47,6 @@ def addToNSUIDs(region: str):
     json.dump(NSUIDs, file, indent="\t", ensure_ascii=False)
     file.close()
 
+os.makedirs("ValidNsuIds", exist_ok=True)
 with ThreadPoolExecutor(max_workers=2) as executor:
     executor.map(addToNSUIDs, REGIONS)
