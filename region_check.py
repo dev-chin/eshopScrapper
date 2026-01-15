f83a1a2c8fd3d89a7689b99067750f9eda38cbce
dev-chin <59340654+dev-chin@users.noreply.github.com>
2026-01-15 09:47:04 +0100

    Add files via upload

---
+++ region_check.py
@@ -0,0 +1,47 @@
+import json
+import requests
+from concurrent.futures import ThreadPoolExecutor
+import sys
+
+REGIONS = ["BG", "CH", "CY", "EE", "HR", "IE", "LT", "LU", "LV", "MT", "RO", "SI", "SK", "AT", "BE", "CZ", "DK", "ES", "FI", "GR", "HU", "NL", "NO", "PL", "PT", "RU", "ZA", "SE", "IT", "FR", "DE", "GB", "TH", "KR"]
+
+region = ""
+
+OUTPUT = {}
+
+def checkTitleid(titleid: str):
+    url = f"https://ec.nintendo.com/apps/{titleid}/{region}"
+    with requests.head(url, stream=True, allow_redirects=False) as response:
+        status_code = response.status_code
+        if (status_code == 303):
+            OUTPUT[f"{titleid}"].append(region)
+            print(f"✓ {region} {titleid}")
+        elif (status_code == 403):
+            print("✗ Hit rate limit. Aborting...")
+            sys.exit(1)
+        else: 
+            print(f"✗ {region} {titleid}: {status_code}")
+
+with open("output/main_regions_alt.json", "r", encoding="UTF-8") as f:
+    titleids = list(json.load(f).keys())
+
+try:
+    with open("output/main_regions_alt2.json", "r", encoding="UTF-8") as f:
+        OUTPUT = list(json.load(f))
+except:
+    pass
+
+checkedtitleids = list(OUTPUT.keys())
+
+titleids[:] = [x for x in titleids if x not in checkedtitleids]
+
+for x in range(len(titleids)):
+    OUTPUT[f"{titleids[x]}"] = []
+
+for m_region in REGIONS:
+    region = m_region
+    with ThreadPoolExecutor(max_workers=1) as executor: #Setting more is risky because rate limits can kick in
+        executor.map(checkTitleid, titleids)
+
+with open("output/main_regions_alt2.json", "w", encoding="UTF-8") as f:
+    json.dump(OUTPUT, f)
