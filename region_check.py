ecb225e861785060fdb98349d69826377356b320
dev-chin <59340654+dev-chin@users.noreply.github.com>
2026-01-16 15:16:23 +0100

    Update region_check.py

--- region_check.py
+++ region_check.py
@@ -14,13 +14,14 @@ def checkTitleid(titleid: str):
     with requests.head(url, stream=True, allow_redirects=False) as response:
         status_code = response.status_code
         if (status_code == 303):
-            OUTPUT[f"{titleid}"].append(region)
+            OUTPUT[f"{titleid}"]["True"].append(region)
             print(f"✓ {region} {titleid}")
         elif (status_code == 403):
             print("✗ Hit rate limit. Aborting...")
             sys.exit(1)
         else: 
             print(f"✗ {region} {titleid}: {status_code}")
+            OUTPUT[f"{titleid}"]["False"].append(region)
 
 with open("output/main_regions_alt.json", "r", encoding="UTF-8") as f:
     titleids = list(json.load(f).keys())
@@ -36,7 +37,7 @@ checkedtitleids = list(OUTPUT.keys())
 titleids[:] = [x for x in titleids if x not in checkedtitleids]
 
 for x in range(len(titleids)):
-    OUTPUT[f"{titleids[x]}"] = []
+    OUTPUT[f"{titleids[x]}"] = {"True": [], "False": []}
 
 for m_region in REGIONS:
     region = m_region
@@ -44,6 +45,4 @@ for m_region in REGIONS:
         executor.map(checkTitleid, titleids)
 
 with open("output/main_regions_alt2.json", "w", encoding="UTF-8") as f:
-
     json.dump(OUTPUT, f)
-
